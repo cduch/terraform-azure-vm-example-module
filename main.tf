dd9f481f65e9657f175905252bde92caace6dd4a
Carsten Duch <carsten@hashicorp.com>
2021-06-08 11:38:52 +0200

    added provider requirement and readme

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  required_version = ">=0.14.9"
+}
 
 /*
     Resource Group
